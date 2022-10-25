f2bcf9c12db74b3ebf77f2fbd012eb1a6963d564
tjlinch <tjlinch@gmail.com>
2022-10-25 11:18:25 -0600

    3-1 arithmetic

--- 3-Algorithms/01-arithmetic/Unsolved/arithmetic.js
+++ 3-Algorithms/01-arithmetic/Unsolved/arithmetic.js
@@ -1,9 +1,22 @@
 // Fill in the body of each function with the code required
 
-var add = function(num1, num2) {};
+var add = function(num1, num2) {
+    sum = num1 + num2;
+    return sum;
+};
 
-var subtract = function(num1, num2) {};
+var subtract = function(num1, num2) {
+    difference = num1 - num2;
+    return difference;
+};
 
-var multiply = function(num1, num2) {};
+var multiply = function(num1, num2) {
+    product = num1 * num2;
+    return product;
+};
 
-var divide = function(num1, num2) {};
+var divide = function(num1, num2) {
+    quotient = num1 / num2;
+    return quotient;
+
+};
