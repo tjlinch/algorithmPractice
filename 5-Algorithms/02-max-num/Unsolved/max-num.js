98fd3e723bc4f151fc6ec7c2e2e28d27bc654cc7
tjlinch <tjlinch@gmail.com>
2022-10-25 12:18:51 -0600

    5-2 max-num

--- 5-Algorithms/02-max-num/Unsolved/max-num.js
+++ 5-Algorithms/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,11 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+    let highNum = 0;
+    for( i = 0; i < arr.length; i++ ) {
+        if ( arr[i] > highNum ) {
+            highNum = arr[i];
+        };
+    };
+    return highNum;
+};
