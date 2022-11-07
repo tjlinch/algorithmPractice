5aaaead2c106cfb2a6a5896e9e913bcc679fcf07
tjlinch <tjlinch@gmail.com>
2022-11-07 10:28:28 -0700

    6-3 title-case

--- 6-Algorithms/03-title-case/Unsolved/title-case.js
+++ 6-Algorithms/03-title-case/Unsolved/title-case.js
@@ -1,3 +1,6 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function(str) {
+    const text = str.toLowerCase().split(' ').map((words) => words.charAt(0).toUpperCase() + words.substring(1)).join(' ');
+    return text;
+};
