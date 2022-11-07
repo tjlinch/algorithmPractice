d34d6c8707f758b7dac47c192be52ebb657979c6
tjlinch <tjlinch@gmail.com>
2022-11-07 10:08:50 -0700

    6-1 is-palindrome

--- 6-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
+++ 6-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,10 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function(str) {
+    const reverseStr = str.split('').reverse().join('');
+    if ( str === reverseStr ) {
+        return true;
+    } else {
+        return false;
+    }
+};
