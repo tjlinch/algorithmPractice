dc95cd1344fd6c9fe7489e2b9bf59c08a534cc62
tjlinch <tjlinch@gmail.com>
2022-11-07 10:03:42 -0700

    5-3 vowel-count

--- 5-Algorithms/03-vowel-count/Unsolved/vowel-count.js
+++ 5-Algorithms/03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,12 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+    
+    if (!str) {
+        return 0;
+    }
+    
+    const count = str.match(/[aeiou]/gi).length;
+
+    return count;
+};
