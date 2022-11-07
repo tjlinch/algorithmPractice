5fbf34ee578bb9509178ec9c2b75d9434d4bc234
tjlinch <tjlinch@gmail.com>
2022-11-07 10:20:20 -0700

    6-2 factorial

--- 6-Algorithms/02-factorial/Unsolved/factorial.js
+++ 6-Algorithms/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,9 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function(num) {
+    if ( num === 0 || num === 1 ) {
+        return 1;
+    } else {
+        return num * factorial(num-1);
+    }
+};
