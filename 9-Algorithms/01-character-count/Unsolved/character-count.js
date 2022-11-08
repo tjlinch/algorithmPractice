26284099b489fc97e73501d35b55f09bafd1bdf5
tjlinch <tjlinch@gmail.com>
2022-11-08 14:24:51 -0700

    9-1 character-count

--- 9-Algorithms/01-character-count/Unsolved/character-count.js
+++ 9-Algorithms/01-character-count/Unsolved/character-count.js
@@ -1,3 +1,18 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
-var characterCount = function(str) {};
+var characterCount = function(str) {
+    const counts = {};
+    const strArr = str.split('');
+    
+    for ( i = 0; i < strArr.length; i++ ) {
+        let char = strArr[i];
+        // console.log(char);
+        if (char in counts) {
+            counts[char] = counts[char] + 1;
+        } else {
+            counts[char] = 1;
+        }
+    }
+    
+    return counts;
+};
