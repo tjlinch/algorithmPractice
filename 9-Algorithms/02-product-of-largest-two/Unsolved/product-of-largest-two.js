378536729188d6d57103e2539945254909fb228f
tjlinch <tjlinch@gmail.com>
2022-11-08 21:25:59 -0700

    9-2 product-of-largest-two

--- 9-Algorithms/02-product-of-largest-two/Unsolved/product-of-largest-two.js
+++ 9-Algorithms/02-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,3 +1,9 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
-var productOfLargestTwo = function(arr) {};
+var productOfLargestTwo = function(arr) {
+    
+    const sortedArr = arr.sort(function(a, b){return a-b});
+    const product = sortedArr[arr.length -1] * sortedArr[arr.length - 2];
+    return product;
+
+};
