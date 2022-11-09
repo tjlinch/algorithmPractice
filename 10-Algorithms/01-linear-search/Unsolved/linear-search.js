1b75a92b49fead856252b8c00e6ad17c96c4e31f
tjlinch <tjlinch@gmail.com>
2022-11-09 09:31:09 -0700

    10-1 linear search

--- 10-Algorithms/01-linear-search/Unsolved/linear-search.js
+++ 10-Algorithms/01-linear-search/Unsolved/linear-search.js
@@ -3,4 +3,12 @@
 // else return -1
 // You may not use the `indexOf`, `lastIndexOf` or `includes` methods
 
-var linearSearch = function(arr, target) {};
+var linearSearch = function(arr, target) {
+    if (target in arr) {
+        for ( i = 0; i < arr.length; i++) {
+            if (arr[i] === target) {
+                return i;
+            }
+        }
+    } return -1;
+};
