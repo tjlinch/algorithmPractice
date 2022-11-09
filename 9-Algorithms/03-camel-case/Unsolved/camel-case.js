b3567a1dc3b2110433b8d403e849b0bf0809333f
tjlinch <tjlinch@gmail.com>
2022-11-09 09:23:10 -0700

    9-3 camel-case

--- 9-Algorithms/03-camel-case/Unsolved/camel-case.js
+++ 9-Algorithms/03-camel-case/Unsolved/camel-case.js
@@ -1,3 +1,7 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
-var camelCase = function(str) {};
+var camelCase = function(str) {
+    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(match, index) {
+        return index === 0 ? match.toLowerCase() : match.toUpperCase();
+    }).replace(/\s+/g, "");
+};
