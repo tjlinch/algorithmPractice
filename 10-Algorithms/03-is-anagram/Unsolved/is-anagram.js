089b82fff9de3ddfc2108254deb8128514b25110
tjlinch <tjlinch@gmail.com>
2022-11-09 09:47:56 -0700

    10-3 is-anagram

--- 10-Algorithms/03-is-anagram/Unsolved/is-anagram.js
+++ 10-Algorithms/03-is-anagram/Unsolved/is-anagram.js
@@ -1,4 +1,12 @@
 // Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
 
-var isAnagram = function(strA, strB) {};
+var isAnagram = function(strA, strB) {
+
+    const sortedStrA = strA.split("").sort().join("");
+    const sortedStrB = strB.split("").sort().join("");
+
+    if (sortedStrA === sortedStrB) {
+        return true;
+    } return false;
+};
