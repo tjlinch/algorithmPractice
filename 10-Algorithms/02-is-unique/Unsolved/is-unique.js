d30786a26e324bc87035a8036d412056da025ae4
tjlinch <tjlinch@gmail.com>
2022-11-09 09:37:33 -0700

    10-2 is-unique

--- 10-Algorithms/02-is-unique/Unsolved/is-unique.js
+++ 10-Algorithms/02-is-unique/Unsolved/is-unique.js
@@ -1,4 +1,11 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+var isUnique = function(arr) {
+    sortedArr = arr.sort();
+    for( i = 0; i < sortedArr.length; i++) {
+        if (sortedArr[i] === sortedArr[i-1]) {
+            return false;
+        }
+    } return true;
+};
