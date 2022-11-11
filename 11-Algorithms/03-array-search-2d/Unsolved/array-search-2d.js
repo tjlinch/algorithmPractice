2b49cf31b348cfd3772e093317bcc176001bc465
tjlinch <tjlinch@gmail.com>
2022-11-10 18:58:28 -0700

    11-3 array-search-2d

--- 11-Algorithms/03-array-search-2d/Unsolved/array-search-2d.js
+++ 11-Algorithms/03-array-search-2d/Unsolved/array-search-2d.js
@@ -2,4 +2,13 @@
 // Each 2D element contains either the string "X" or "O" (both capitalized)
 // Return the number of times "X" appears in the 2D array
 
-var arraySearch2D = function(arr) {};
+var arraySearch2D = function(arr) {
+    let xCount = 0;
+    for ( i = 0; i < arr.length; i++ ) {
+        for ( j = 0; j < arr[i].length; j++ ) {
+            if (arr[i][j] === 'X') {
+                xCount = xCount + 1;
+            }
+        }
+    } return xCount;
+};
