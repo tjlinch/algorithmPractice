cc409e8320c05facc2467e7381c60413b178d368
tjlinch <tjlinch@gmail.com>
2022-11-11 15:56:27 -0700

    12-2 zeroes-and-ones

--- 12-Algorithms/02-zeroes-and-ones/Unsolved/zeroes-and-ones.js
+++ 12-Algorithms/02-zeroes-and-ones/Unsolved/zeroes-and-ones.js
@@ -2,4 +2,28 @@
 // Return true if there are an equal number of 0s and 1s
 // Else return false
 
-var zeroesAndOnes = function(str) {};
+var zeroesAndOnes = function(str) {
+    // split the string into an array so we can iterate trhough it
+    const strArr = str.split('');
+
+    // set count variables to 0
+    let count0 = 0;
+    let count1 = 0;
+    
+    for ( i = 0; i < strArr.length; i++ ) {
+        
+        // while iterating through the string array, update the count variables
+        if (strArr[i] === '0') {
+            count0 = count0 + 1;
+        } else {
+            count1 = count1 + 1;
+        }
+    }
+
+    // compare the two count variables. If they are equal, return true. Else return false.
+    if ( count0 === count1 ) {
+        return true;
+    } else {
+        return false;
+    }
+};
