aea3031882e8d8f07dc175fb0cf4c5b7ed673ec7
tjlinch <tjlinch@gmail.com>
2022-11-11 16:04:52 -0700

    12-3 merge-sorted

--- 12-Algorithms/03-merge-sorted/Unsolved/merge-sorted.js
+++ 12-Algorithms/03-merge-sorted/Unsolved/merge-sorted.js
@@ -1,3 +1,10 @@
 // Write code to merge two sorted arrays into a new sorted array
 
-var mergeSorted = function (arr1, arr2) {};
+var mergeSorted = function (arr1, arr2) {
+    
+    // declare a new array containing the two given arrays using the spread operator.
+    const mergedArr = [...arr1, ...arr2];
+
+    // return the sorted merged array using the .sort() method with a compare function
+    return mergedArr.sort(function(a, b){return a-b});
+};
