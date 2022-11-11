ec008d93f5024aaaf44c38cf73267e91fa30ecca
tjlinch <tjlinch@gmail.com>
2022-11-10 18:06:28 -0700

    11-1 most-songs-in-playlist

--- 11-Algorithms/01-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
+++ 11-Algorithms/01-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
@@ -3,5 +3,21 @@
 // Return the maximum number of songs from the list that can fit in a 60 minute playlist
 
 var mostSongsInPlaylist = function(arr) {
-  // add your code here...
+  const sortedArr = arr.sort();
+  let minutes = 0;
+  
+  // if the sum of the array of numbers is less than 60, will return the length of the array
+  if ( sortedArr.reduce((a, b) => a + b, 0) < 60 ) {
+    return sortedArr.length;
+  }
+
+  // loop through the array until minutes is greater than or equal to 60, then returns the index position
+  for ( i = 0; i < sortedArr.length; i++ ) {
+    if ( minutes < 60 ) {
+      minutes = minutes + sortedArr[i];
+    } else if ( minutes > 60 ) {
+      minutes = minutes - sortedArr[i];
+      return i - 1;
+    } else return i;
+  }
 };
