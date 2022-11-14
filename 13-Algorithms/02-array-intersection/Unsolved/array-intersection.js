856a130aea1607cb8ac438a2d437fed98d545d82
tjlinch <tjlinch@gmail.com>
2022-11-14 09:42:25 -0700

    13-2 array-intersection

--- 13-Algorithms/02-array-intersection/Unsolved/array-intersection.js
+++ 13-Algorithms/02-array-intersection/Unsolved/array-intersection.js
@@ -1,4 +1,14 @@
 // Write code to create a function that accepts two arrays of numbers
 // Return a new array containing the intersecting elements of the arrays
 
-var arrayIntersection = function(arr1, arr2) {};
+var arrayIntersection = function(arr1, arr2) {
+    const intersectingArr = [];
+
+    for ( i = 0; i < arr1.length; i++ ) {
+        if (arr2.includes(arr1[i])) {
+            intersectingArr.push(arr1[i]);
+        }
+    }
+
+    return intersectingArr;
+};
