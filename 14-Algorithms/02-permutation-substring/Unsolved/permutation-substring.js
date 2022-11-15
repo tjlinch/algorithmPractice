6741704f843f26bc9234d57c8b6b3b55ab38d18b
tjlinch <tjlinch@gmail.com>
2022-11-15 11:56:36 -0700

    14-2 permutation-substring

--- 14-Algorithms/02-permutation-substring/Unsolved/permutation-substring.js
+++ 14-Algorithms/02-permutation-substring/Unsolved/permutation-substring.js
@@ -2,4 +2,17 @@
 // Return true if the second string is a substring of any permutation of the first string
 // Else return false
 
-var permutationSubstring = function(str, sub) {};
+var permutationSubstring = function(str, sub) {
+    // split the substring into an array to perform a for loop
+    const subArr = sub.split('');
+
+    // in the loop, if an element in the substring is not included in the string, return flase;
+    for ( i = 0; i < subArr.length; i++ ){
+        if (!str.includes(subArr[i])){
+            return false;
+        }
+    }
+
+    // if we exit the for loop without hitting the condition, return true.
+    return true;
+};
