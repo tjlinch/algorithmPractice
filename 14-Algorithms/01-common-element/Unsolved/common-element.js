27c570c6a8f4219efcf992a43434d02866587825
tjlinch <tjlinch@gmail.com>
2022-11-15 11:46:02 -0700

    14-1 common-element

--- 14-Algorithms/01-common-element/Unsolved/common-element.js
+++ 14-Algorithms/01-common-element/Unsolved/common-element.js
@@ -3,4 +3,14 @@
 // Return the common number
 // You may not use the `indexOf` or `includes` method
 
-var commonElement = function(arrA, arrB) {};
+var commonElement = function(arrA, arrB) {
+    // merge and sort the two arrays to find if two elements next to eachother are strictly equal.
+    const mergedArr = [...arrA, ...arrB].sort((a, b) => a - b);
+    
+    // loop through the merged array. If an element of the array is strictly equal to the element that came before, return that element.
+    for ( i = 0; i < mergedArr.length; i++ ) {
+        if (mergedArr[i] === mergedArr[i-1]) {
+            return mergedArr[i];
+        }
+    }
+};
