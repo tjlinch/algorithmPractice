955a93a82e044ac9f4c214ae0c98d7d1bcf6c1bc
tjlinch <tjlinch@gmail.com>
2022-11-15 12:07:41 -0700

    14-3 concert-flyer

--- 14-Algorithms/03-concert-flyer/Unsolved/concert-flyer.js
+++ 14-Algorithms/03-concert-flyer/Unsolved/concert-flyer.js
@@ -1,3 +1,17 @@
 // Write a function that takes two strings and returns true if every word found in the second string is present in the first string. You will be checking for both words and their frequency. Assume you'll need to worry about casing, but the strings won't contain any punctuation. Assume neither string will be empty
 
-var concertFlyer = function (magazine, flyer) {};
+var concertFlyer = function (magazine, flyer) {
+    // convert the magazine and flyer strings into arrays to use a loop and the .includes method.
+    const flyerArr = flyer.toLowerCase().split(' ');
+    const magazineArr = magazine.toLowerCase().split(' ');
+
+    // loop through the flyer array and if an element is not included in the magazine string return false.
+    for ( i = 0; i < flyerArr.length; i++ ) {
+        if (!magazineArr.includes(flyerArr[i])){
+            return false;
+        }
+    }
+
+    // if we exit the for loop without hitting the condition, return true.
+    return true;
+};
